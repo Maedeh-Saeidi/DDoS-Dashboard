cd166ba1bce6eca1d971ddcb95763a9fc025dffd
maedeh <saeidimdhw@gmail.com>
2025-05-18 18:30:26 +0330

    WIP:Working on maximum duration component

--- src/components/maximumAttackDurtion.tsx
+++ src/components/maximumAttackDurtion.tsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function MaximumAttackDurtion() {
   return (
-    <div className='min-w-[50%] h-auto bg-blue-300'>maximumAttackDurtion</div>
+    <div className='flex flex-row gap-5  bg-[#0F172A] p-5 border border-[#1E293B]'>
+      {/* search svg  */} 
+     <div className='flex flex-row justify-center align-center'>
+      {/* clock */}
+       <div className='flex flex-col align-start justify-start'>
+        <div className='text-[#78FF97] font-medium text-2xl'>Maximum <span className='block text-[#FFFFFF]'> Attack Duration</span>
+        </div>
+      </div>
+     </div>
+    </div>
   )
 }
